5e8d4a14ffe6b040d32a3a590c7c85ff2df504c2
Alejandro Santa <alejo@lamusica.fm>
2026-09-04 18:22:46 -0500

    Taller 1 ejercicio 3

---
+++ Taller1-Lineales/ejercicio3.js
@@ -0,0 +1,16 @@
+// Ejercicio 3 - Operaciones basicas
+
+let numero1 = parseFloat(prompt("Ingrese el primer numero"));
+let numero2 = parseFloat(prompt("Ingrese el segundo numero"));
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+let modulo = numero1 % numero2;
+
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicacion: " + multiplicacion);
+console.log("Division: " + division);
+console.log("Modulo: " + modulo);
