a31b79df8affb72cf960f36f45f4330b13b23c40
Alejandro Santa <alejo@lamusica.fm>
2026-09-04 18:22:41 -0500

    Taller 1 ejercicio 2

---
+++ Taller1-Lineales/ejercicio2.js
@@ -0,0 +1,8 @@
+// Ejercicio 2 - Area de un triangulo
+
+let base = parseFloat(prompt("Ingrese la base del triangulo"));
+let altura = parseFloat(prompt("Ingrese la altura del triangulo"));
+
+let area = (base * altura) / 2;
+
+console.log("El area del triangulo es: " + area);
