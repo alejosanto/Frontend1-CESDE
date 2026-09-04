e0847ed9612c3c5bb896f07f4c277f4dc1d865ff
Alejandro Santa <alejo@lamusica.fm>
2026-09-04 18:27:43 -0500

    Agregar ejercicio 3 de condicionales

---
+++ Taller2-Condicionales/ejercicio3.js
@@ -0,0 +1,29 @@
+// Ejercicio 3 - Día de la semana
+
+let dia = Number(prompt("Ingrese un número del 1 al 7"));
+
+switch (dia) {
+    case 1:
+        alert("Lunes");
+        break;
+    case 2:
+        alert("Martes");
+        break;
+    case 3:
+        alert("Miércoles");
+        break;
+    case 4:
+        alert("Jueves");
+        break;
+    case 5:
+        alert("Viernes");
+        break;
+    case 6:
+        alert("Sábado");
+        break;
+    case 7:
+        alert("Domingo");
+        break;
+    default:
+        alert("Número no válido");
+}
