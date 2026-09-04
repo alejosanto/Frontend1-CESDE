8ce42ff4a1783fad5947a229e37b5821c5b191f1
Alejandro Santa <alejo@lamusica.fm>
2026-09-04 18:23:02 -0500

    Taller 1 ejercicio 7

---
+++ Taller1-Lineales/ejercicio7.js
@@ -0,0 +1,9 @@
+// Ejercicio 7 - Promedio de 3 notas
+
+let nota1 = parseFloat(prompt("Ingrese la primera nota de 1 a 5"));
+let nota2 = parseFloat(prompt("Ingrese la segunda nota de 1 a 5"));
+let nota3 = parseFloat(prompt("Ingrese la tercera nota de 1 a 5"));
+
+let promedio = (nota1 + nota2 + nota3) / 3;
+
+console.log("El promedio de las notas es: " + promedio);
