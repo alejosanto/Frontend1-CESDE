210ed788584b306358adef59cc99d336b5cec5f8
Alejandro Santa <alejo@lamusica.fm>
2026-09-04 18:22:54 -0500

    Taller 1 ejercicio 5

---
+++ Taller1-Lineales/ejercicio5.js
@@ -0,0 +1,11 @@
+// Ejercicio 5 - Precio de un producto e IVA
+
+let precio = parseFloat(prompt("Ingrese el precio del producto sin IVA"));
+let porcentajeIva = parseFloat(prompt("Ingrese el porcentaje de IVA"));
+
+let iva = precio * (porcentajeIva / 100);
+let total = precio + iva;
+
+console.log("Precio sin IVA: " + precio);
+console.log("IVA agregado: " + iva);
+console.log("Total a pagar: " + total);
